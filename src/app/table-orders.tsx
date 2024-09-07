576f6a07bfac42146f50ab5914de290b860598d9
Eduardo Sávio <saviodudu79@gmail.com>
2024-09-07 09:57:26 -0300

    feat: included total of filtered orders in home, actions column without features

--- src/app/table-orders.tsx
+++ src/app/table-orders.tsx
@@ -8,8 +8,17 @@ import {
   TableBody,
   TableCell,
   Table,
+  TableFooter,
 } from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { db } from "@/constants";
+import { formatBRL } from "@/utils/format-brl";
+import { Eye, FileText } from "lucide-react";
 import { useState } from "react";
 
 interface TableOrdersProps {}
@@ -40,20 +49,55 @@ export const TableOrders = (props: TableOrdersProps) => {
         <Table>
           <TableHeader>
             <TableRow className="text-lg border-b">
+              <TableHead>Ações</TableHead>
               <TableHead>Data</TableHead>
               <TableHead>Cliente</TableHead>
-              <TableHead>Total</TableHead>
+              <TableHead className="text-right">Total</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {rows.map((row) => (
               <TableRow key={row.id} className="text-lg">
+                <TableCell>
+                  <span className="flex items-center gap-2">
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger type="button">
+                          <FileText className="w-4 h-4 stroke-[1.25px] ease-linear transition-all hover:text-blue-700 cursor-pointer" />
+                        </TooltipTrigger>
+                        <TooltipContent className="w-fit text-lg">
+                          <p className="text-center">Exportar</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger type="button">
+                          <Eye className="w-4 h-4 stroke-[1.25px] ease-linear transition-all hover:text-blue-700 cursor-pointer" />
+                        </TooltipTrigger>
+                        <TooltipContent className="w-fit text-lg">
+                          <p className="text-center">Ver pedido completo</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </span>
+                </TableCell>
                 <TableCell>{row.date}</TableCell>
                 <TableCell>{row.customer}</TableCell>
-                <TableCell>{row.total}</TableCell>
+                <TableCell className="text-right">
+                  {formatBRL(row.total)}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
+          <TableFooter>
+            <TableRow className="text-lg">
+              <TableCell colSpan={3}></TableCell>
+              <TableCell className="text-right">
+                {formatBRL(rows.reduce((acc, row) => acc + row.total, 0))}
+              </TableCell>
+            </TableRow>
+          </TableFooter>
         </Table>
       </Card>
     </div>

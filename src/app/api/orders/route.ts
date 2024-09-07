bdea2451a041536d4e8ec2e77a4dc4d6e8ee3604
Eduardo Sávio <saviodudu79@gmail.com>
2024-09-07 15:18:34 -0300

    feat: testing fs file sync

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,25 +1,28 @@
-import { IOrder, orders } from "@/db";
+import { orders } from "@/db";
+import db from "@/json/db.json";
+import fs from "fs";
 
-export async function GET(request: Request): Promise<IOrder[]> {
-  return orders;
+export async function GET(request: Request) {
+  return Response.json(orders);
 }
 
-export async function POST(request: Request): Promise<IOrder> {
+export async function POST(request: Request) {
   const order = await request.json();
-  orders.push(order);
-  return order;
+  db.orders.push(order);
+  fs.writeFileSync("./src/json/db.json", JSON.stringify(db, null, 2));
+  return Response.json(order);
 }
 
-export async function PUT(request: Request): Promise<IOrder> {
+export async function PUT(request: Request) {
   const order = await request.json();
   const index = orders.findIndex((o) => o.id === order.id);
   orders[index] = order;
-  return order;
+  return Response.json(order);
 }
 
-export async function DELETE(request: Request): Promise<IOrder> {
+export async function DELETE(request: Request) {
   const order = await request.json();
   const index = orders.findIndex((o) => o.id === order.id);
   orders.splice(index, 1);
-  return order;
+  return Response.json(order);
 }
